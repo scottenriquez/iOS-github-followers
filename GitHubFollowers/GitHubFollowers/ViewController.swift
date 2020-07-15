51dc3c4bab5379b2c9e9f975d39a1300aa82b927
Scottie Enriquez <scottie@enriquez.xxx>
2020-07-15 18:07:18 -0500

    Adding programmatic template; adding app icon and image assets

---
+++ GitHubFollowers/GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Scott Enriquez on 7/15/20.
+//  Copyright © 2020 Scott Enriquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
