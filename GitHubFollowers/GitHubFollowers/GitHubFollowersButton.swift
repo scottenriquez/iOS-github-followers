7d5dfa05f2814fea0e93b59977bc6e5c2afc68b9
Scottie Enriquez <scottie@enriquez.xxx>
2020-07-20 22:42:22 -0500

    Adding custom UI button and text field subclasses

---
+++ GitHubFollowers/GitHubFollowers/GitHubFollowersButton.swift
@@ -0,0 +1,35 @@
+//
+//  GitHubFollowersButton.swift
+//  GitHubFollowers
+//
+//  Created by Scott Enriquez on 7/20/20.
+//  Copyright © 2020 Scott Enriquez. All rights reserved.
+//
+
+import UIKit
+
+class GitHubFollowersButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GitHubFollowers/GitHubFollowers/GitHubUserSearchTextField.swift
@@ -0,0 +1,37 @@
+//
+//  GitHubUserSearchTextField.swift
+//  GitHubFollowers
+//
+//  Created by Scott Enriquez on 7/20/20.
+//  Copyright © 2020 Scott Enriquez. All rights reserved.
+//
+
+import UIKit
+
+class GitHubUserSearchTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        textColor = .label
+        textAlignment = .center
+        tintColor = .label
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
