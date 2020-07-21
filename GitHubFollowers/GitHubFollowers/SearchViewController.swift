c85909466f7e69a1290baba4c677d394fe96eaf9
Scottie Enriquez <scottie@enriquez.xxx>
2020-07-21 11:49:54 -0500

    Adding home screen UI

--- GitHubFollowers/GitHubFollowers/SearchViewController.swift
+++ GitHubFollowers/GitHubFollowers/SearchViewController.swift
@@ -10,9 +10,53 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let logoImageView = UIImageView()
+    let usernameTextField = GitHubUserSearchTextField()
+    let getFollowersButton = GitHubFollowersButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureUsernameTextField()
+        configureGetFollowersButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.image = UIImage(named: "gh-logo")!
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    func configureUsernameTextField() {
+        view.addSubview(usernameTextField)
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50)
+        ])
+    }
+    
+    func configureGetFollowersButton() {
+        view.addSubview(getFollowersButton)
+        NSLayoutConstraint.activate([
+            getFollowersButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            getFollowersButton.heightAnchor.constraint(equalToConstant: 50),
+            getFollowersButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            getFollowersButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50)
+        ])
     }
     
 }
